55a960f9aa2052b23cb4f0eb73bf8b040ab5f885
FIX 11 <fix11@example.com>
2026-05-10 20:16:28 +0300

    Fix PrismaService and move @types/node to dependencies

--- backend/src/prisma.service.ts
+++ backend/src/prisma.service.ts
@@ -1,13 +1,10 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor(private configService: ConfigService) {
-    super({
-      datasourceUrl: configService.get('DATABASE_URL'),
-    });
+  constructor() {
+    super();
   }
 
   async onModuleInit() {
